ccf2fb2bc297f062e84b933306531b4809e4a97c
Rhaegall <corentinmistere@gmail.com>
2018-04-11 10:18:26 +0200

    Envoi welcome_page1.php et identify.php

---
+++ identify.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Identify</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<header>
+		<div class="header">Header</div>
+	</header>
+		<div class="identify">Je m'identifie</div>
+		<div class="fb">
+			<input type="submit" value="Connectez-vous en tant que">
+		</div>
+		<div class="choice">---------- Ou ----------</div>
+		<div class="mail">Adresse mail</div>
+		<div class="ml">
+			<input type="text" required placeholder="example@ex.fr">
+		</div>
+		<div class="pass">Mot de passe</div>
+		<div class="pswd">
+			<input type="text" required placeholder="ExAmPlE">
+		</div>
+		<div class="connect">
+			<input type="submit" value="Connexion">
+		</div>
+		<div class="noAccount">Vous n'avez pas de compte?</div>
+	<footer>
+		<div class="footer">Footer</div>
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ welcome_page1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome_Page1</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+</head>
+<body>
+	<header>
+		<div class="header">Header</div>
+	</header>
+		<div class="prgp">
+			<p>Envie d'un voyage? Créez votre cagnotte et collectez<br /> pour partir à l'endroit de vos rêves</p>
+		</div>
+		<div class="pg">
+			<img id="pig" src="css/img/cocochon.jpg" alt="Image cochon tirelire">
+		</div>
+		<div class="btnG">
+			<input type="submit" value="Je crée ma cagnotte">
+		</div>
+		<div class="btnD">
+			<input type="submit" value="Je participe à une cagnotte">
+		</div>
+		</div>	
+	<footer>
+		<div class="footer">Footer</div>
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
